44ae58077bfe79d9732cb1067c768bd717a8436c
Chaitali <chaitali6112002@gmail.com>
2025-09-12 10:30:17 +0530

    Fix SecurityConfig and JwtRequestFilter, updated application.properties for Render deployment

--- src/main/java/com/main/config/SecurityConfig.java
+++ src/main/java/com/main/config/SecurityConfig.java
@@ -36,7 +36,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth.requestMatchers("/status","/health","/register","/activate","/login").permitAll()
+//                .authorizeHttpRequests(auth->auth.requestMatchers("/status","/health","/register","/activate","/login").permitAll()
+                .authorizeHttpRequests(auth->auth.requestMatchers(
+                                "/api/v1.0/login",
+                                "/api/v1.0/register",
+                                "/api/v1.0/activate",
+                                "/api/v1.0/status",
+                                "/api/v1.0/health"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
