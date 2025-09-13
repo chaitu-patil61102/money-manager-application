e19177d50fa9d94105de46031ed8cb66d89ac2bb
Chaitali <chaitali6112002@gmail.com>
2025-09-13 23:36:44 +0530

    second commit

--- src/main/java/com/main/config/SecurityConfig.java
+++ src/main/java/com/main/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth.requestMatchers("/status","/health","/register","/activate","/login").permitAll()
+//                .authorizeHttpRequests(auth->auth.requestMatchers("/status","/health","/register","/activate","/login").permitAll()
+                .authorizeHttpRequests(auth->auth.requestMatchers("/api/v1.0/register","/api/v1.0/activate","/api/v1.0/login").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
